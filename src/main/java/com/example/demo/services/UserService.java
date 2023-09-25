31d3fbe866eaf94a5d71856e8f15a1e95b754733
YaremchukVadym <yaremchuk2003vadym@gmail.com>
2023-09-25 14:52:17 +0200

    feat: Added services and DTO

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 
+import com.example.demo.dto.UserDTO;
 import com.example.demo.exceptions.UserExistExceptions;
 import com.example.demo.payload.request.SignupRequest;
 import com.example.demo.entity.User;
@@ -9,8 +10,11 @@ import com.example.demo.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
 
 @Service
 public class UserService {
@@ -20,13 +24,12 @@ public class UserService {
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder)
-    {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User createUser(SignupRequest userIn){
+    public User createUser(SignupRequest userIn) {
         User user = new User();
         user.setEmail(userIn.getEmail());
         user.setName(userIn.getFirstname());
@@ -38,11 +41,28 @@ public class UserService {
         try {
             LOG.info("Saving User {}", userIn.getEmail());
             return userRepository.save(user);
-        }catch (Exception e){
+        } catch (Exception e) {
             LOG.error("Error during registration. {}", e.getMessage());
             throw new UserExistExceptions("The user " + user.getUsername() + " already exist. Please check credentials");
         }
+    }
 
+    public User updateUser(UserDTO userDTO, Principal principal) {
+        User user = getUserByPrincipal(principal);
+        user.setName(userDTO.getFirstname());
+        user.setLastname(user.getLastname());
+        user.setBio(userDTO.getBio());
 
+        return userRepository.save(user);
+    }
+
+    public User getCurrentUser(Principal principal){
+        return getUserByPrincipal(principal);
+    }
+
+    private User getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username " + username));
     }
 }
